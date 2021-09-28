a1109776d02b316df34a30c2ce55f08f4ee9d649
John Coakley <johnmcoakley64@gmail.com>
2021-09-28 17:14:06 -0500

    just a few changes

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,9 @@
 //know issues
-//time can go negative, if the the player gets an answer wrong
-//I need to make clicknig the answer, go to the next question
 //todo
-//I need to write the questions
 //Make an ending game function. 
 //What will that do?
 //i need to make local storage, user will input their Name, and it will record their score.
 //come up with a way to score the player.
-
-
-
-
-
-
-
 let playGame = false;
 let gameTimer = 75;
 let optionChosen;
@@ -28,88 +18,93 @@ let rightWrongEl = document.getElementById("right-wrong");
 let startQuizBtn = document.getElementById("start-quiz-btn");
 let nextBtn = document.getElementById("next-btn");
 let question_1 = [];
+let question_2 = [];
+let question_3 = [];
+let question_4 = [];
+let question_5 = [];
 let questionCount = 0;
 let gamesScore = 0;
 
 // how to store the question asnwer and options.
 let gameStuff = [question_1 =
 {
-    Question: "How much would a wood chuck",
-    answer: "option-b",
+    Question: "Commonly used data types DO NOT include:",
+    answer: "option-c",
     option: ["option-a", "option-b", "option-c", "option-d",],
     //corract answer goes here so u can append?? or textcontent to the page
-    optionShown: ["a", "b-The correct answer goes here", "c", "d"]
+    optionShown: ["strings", "booleans", "alerts", "numbers"]
 },
 question_2 =
 {
 
-    Question: "What data type will this line of code be after is is concatenated?",
-    answer: "option-a",
+    Question: "The condition of an if/else statment is enclosed within ____.",
+    answer: "option-c",
     option: ["option-a", "option-b", "option-c", "option-d",],
     //corract answer goes here so u can append?? or textcontent to the page
-    optionShown: ["a: a string", "b: a number", "c: an object", "d. all of the above"]
+    optionShown: ["a: quotes", "b: curly brackets", "c: parentheses", "d. square brackets"]
 },
 
 question_3 =
 {
 
-    Question: "Question 3",
-    answer: "option-a",
+    Question: "A very useful tool used during development and debuggin for printing content to the debugger is:",
+    answer: "option-b",
     option: ["option-a", "option-b", "option-c", "option-d",],
     //corract answer goes here so u can append?? or textcontent to the page
-    optionShown: ["a: a string", "b: a number", "c: an object", "d. all of the above"]
+    optionShown: ["a: JavaScript", "b: console.log ", "c: for loops", "d. terminal / bash"]
 },
 
 question_4 =
 {
 
-    Question: "Wquestion 4",
-    answer: "option-a",
+    Question: "Arrays in JavaScript can be used to store _____",
+    answer: "option-d",
     option: ["option-a", "option-b", "option-c", "option-d",],
     //corract answer goes here so u can append?? or textcontent to the page
-    optionShown: ["a: a string", "b: a number", "c: an object", "d. all of the above"]
+    optionShown: ["a: numbers and strings", "b: other arrays", "c: booleans", "d. all of the above"]
 },
 
 question_5 =
 {
 
-    Question: "question 5",
-    answer: "option-a",
+    Question: "String values must be enclosed within _____ when being assigned to variables",
+    answer: "option-c",
     option: ["option-a", "option-b", "option-c", "option-d",],
     //corract answer goes here so u can append?? or textcontent to the page
-    optionShown: ["asdfsdfds: a string", "b: a sdfsd", "c: an obasdasject", "d. all asdasdof the above"]
+    optionShown: ["a: commas", "b: curly brackets", "c: quotes", "d. parentheses"]
 }
 ]
 
 
 // start game. 
 startQuizBtn.addEventListener("click", function () {
-    
 
-  console.log(playGame)
+    startQuizBtn.setAttribute("style", "visibility: hidden");
 
-playGame = true
 
-    if (playGame == true){
-        console.log(playGame)
-          setTime()
+    playGame = true
+
+    if (playGame == true) {
+        setTime()
     }
 
-   
 
-        quizQuestionEl.innerHTML = gameStuff[questionCount].Question
-        //how do I get the options onto the page?
-        optionAel.innerHTML = gameStuff[questionCount].optionShown[0]
-        optionBel.innerHTML = gameStuff[questionCount].optionShown[1]
-        optionCel.innerHTML = gameStuff[questionCount].optionShown[2]
-        optionDel.innerHTML = gameStuff[questionCount].optionShown[3]
-    
-        if (playGame == false){
-            gameOver()
-            console.log("this means the game over function is going to run")
-        }
- 
+
+    quizQuestionEl.innerHTML = gameStuff[questionCount].Question
+    //how do I get the options onto the page?
+    optionAel.innerHTML = gameStuff[questionCount].optionShown[0]
+    optionBel.innerHTML = gameStuff[questionCount].optionShown[1]
+    optionCel.innerHTML = gameStuff[questionCount].optionShown[2]
+    optionDel.innerHTML = gameStuff[questionCount].optionShown[3]
+
+    if (playGame == false) {
+        gameOver()
+        console.log("this means the game over function is going to run")
+    }
+
 })
+
+
 
 
 // start button will only start with gamestuf[0].
@@ -117,7 +112,7 @@ playGame = true
 //statment if statment for the next btn. if gamestuff = 0 then run the incrmatnation questionsCount++
 //
 // nextBtn.addEventListener("click", function () {
-  
+
 // })
 
 //when the game starts remove the "start button"
@@ -134,41 +129,42 @@ playGame = true
 function checkAnswer() {
     if (gameStuff[0].answer == optionChosen) {  //change this is a variable and base it on what the person chooses.
         addPts()
+        console.log(optionChosen)
         setMessageTimer("Thats right")
-        // how do i get this to pause and show this message before moving to the next question
-        //set a timer for 3 seconds??
-       
-        //put timer here.
+        
+        if (questionCount == 4){
+            gameOver()
+            return
+        }
 
-        
         questionCount++
-       
-            quizQuestionEl.innerHTML = gameStuff[questionCount].Question
-            //how do I get the options onto the page?
-            optionAel.innerHTML = gameStuff[questionCount].optionShown[0]
-            optionBel.innerHTML = gameStuff[questionCount].optionShown[1]
-            optionCel.innerHTML = gameStuff[questionCount].optionShown[2]
-            optionDel.innerHTML = gameStuff[questionCount].optionShown[3]
-        
+
+        quizQuestionEl.innerHTML = gameStuff[questionCount].Question
+        //how do I get the options onto the page?
+        optionAel.innerHTML = gameStuff[questionCount].optionShown[0]
+        optionBel.innerHTML = gameStuff[questionCount].optionShown[1]
+        optionCel.innerHTML = gameStuff[questionCount].optionShown[2]
+        optionDel.innerHTML = gameStuff[questionCount].optionShown[3]
+
     } else {
-    setMessageTimer("you suck")
-        
+        setMessageTimer("you suck")
+
         console.log("you suck")
         losePts()
     }
 
-    function setMessageTimer(message){
+    function setMessageTimer(message) {
         var messageTimer = 3
-        var messageInterval = setInterval(function(){
+        var messageInterval = setInterval(function () {
             messageTimer--
             rightWrongEl.textContent = message
-        
-        
-            if (messageTimer < 1){
+
+
+            if (messageTimer < 1) {
                 clearInterval(messageInterval)
-                
+
                 rightWrongEl.textContent = ""
-                
+
             }
         }, 1000)
     }
@@ -176,91 +172,17 @@ function checkAnswer() {
 
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// nextBtn.addEventListener("click", function () {
-//     questionCount++
-//     rightWrongEl.textContent = ""
-//         quizQuestionEl.innerHTML = gameStuff[questionCount].Question
-//         //how do I get the options onto the page?
-//         optionAel.innerHTML = gameStuff[questionCount].optionShown[0]
-//         optionBel.innerHTML = gameStuff[questionCount].optionShown[1]
-//         optionCel.innerHTML = gameStuff[questionCount].optionShown[2]
-//         optionDel.innerHTML = gameStuff[questionCount].optionShown[3]
-    
-
-// })
-
-
-// function checkAnswer() {
-//     if (gameStuff[0].answer == optionChosen) {  //change this is a variable and base it on what the person chooses.
-//         addPts()
-//         rightWrongEl.textContent = "Thats right!"
-        
-//     } else {
-//         rightWrongEl.textContent = "You suck"
-//         console.log("you suck")
-//         losePts()
-//     }
-// }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//how do i get the quetion from the array game Stuff on to thewebpage?
-// for (let i = 0; i < gameStuff.length; i++) {
-
-//     quizQuestionEl.innerHTML = gameStuff[i].Question
-//     //how do I get the options onto the page?
-//     optionAel.innerHTML = gameStuff[i].optionShown[0]
-//     optionBel.innerHTML = gameStuff[i].optionShown[1]
-//     optionCel.innerHTML = gameStuff[i].optionShown[2]
-//     optionDel.innerHTML = gameStuff[i].optionShown[3]
-
-//now how do i get them to roate on the page at the right time?
-
-// }
-
-
-
-
-
-
-// if (gameTimer === 0) {
-//     playGame = false
-// } else {
-//     playGame = true
-// }
-
-
-
-
-
-
-//lets get question 1 show up on the page with only js.
-
-
-
-
-// how do I campare the answer to the option chosen?
-// how do I get then question and options display on the page? or console.log
-// I want to compiar answer with any number that is in the array option:
-
-
-//start with index 0 because this is question 1
-// how do iget the answer?  
-
-
-
-
 //Score keeper
 
-function addPts(){
-    gamesScore++
+function addPts() {
+   
     gameScoreEl.textContent = "Total Score: " + gamesScore
 
 }
 
-function losePts(){
+function losePts() {
     gameTimer = gameTimer - 10
-    if (gameTimer < 1){
+    if (gameTimer < 1) {
         gameTimer = 0
         clearInterval(timerInterval)
     }
@@ -275,61 +197,68 @@ function losePts(){
 //buttons for answers 
 optionAel.addEventListener("click", function () {
     optionChosen = "option-a"
+    console.log("this is option a ", optionChosen)
     checkAnswer();
 });
 
 optionBel.addEventListener("click", function () {
     optionChosen = "option-b"
+    console.log("this is option b ", optionChosen)
     checkAnswer();
 });
 
 optionCel.addEventListener("click", function () {
     optionChosen = "option-c"
+    console.log("this is option c ", optionChosen)
     checkAnswer();
 });
 
 optionDel.addEventListener("click", function () {
     optionChosen = "option-d"
+    console.log("this is option d ", optionChosen)
     checkAnswer();
 });
 
 
 
 //example
-function gameOver(){
-    console.log("game function is running")
+function gameOver() {
+    gamesScore = gameTimer
     quizQuestionEl.innerHTML = ""
-    //how do I get the options onto the page?
     optionAel.innerHTML = ""
     optionBel.innerHTML = ""
     optionCel.innerHTML = ""
     optionDel.innerHTML = ""
 }
-  
 
-// 1. back next-btn
-// 2. make a for loop have that for loop trigger the next page to load.
-// 3. i need to make a function to get the next page to load, but not check the answer.
-// how do I get the info on the screen to show ? with javascript.
 
-        function setTime(){
-            var timerInterval = setInterval(function(){
-                gameTimer--;
-                
-                gameTimerel.textContent = "Time Left: " + gameTimer;
-                if (gameTimer < 1){
-                    clearInterval(timerInterval)
-                    playGame = false
+// game timer start
+function setTime() {
+    var timerInterval = setInterval(function () {
+        gameTimer--;
 
-                    if (playGame == false){
-                        gameOver()
+        gameTimerel.textContent = "Time Left: " + gameTimer;
+        if (gameTimer < 1) {
+            clearInterval(timerInterval)
+            playGame = false
 
-                    }
-                    console.log(playGame, "this means the play game var is changing to endgame.")
-                    
-                }
-            }, 1000)
+            if (playGame == false) {
+                gameOver()
+
+            }
+
+
         }
+    }, 1000)
+}
 
 
-        
+console.log("this is the answer for number1", gameStuff[0].answer)
+console.log("this is the answer for number2", gameStuff[1].answer)
+console.log("this is the answer for number3", gameStuff[2].answer)
+console.log("this is the answer for number4", gameStuff[3].answer)
+console.log("this is the answer for number5", gameStuff[4].answer)
+
+
+//end game
+
